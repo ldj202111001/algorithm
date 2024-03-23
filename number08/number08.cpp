51f07bf6a424bc44bc03f8167c269628e618c8e0
ldj202111001 <seon3129@gmail.com>
2024-03-23 13:21:35 +0900

    초기 커밋.

---
+++ number08/number08.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int get_gcd(int min, int max) {		// 최대공약수 구하기 (유클리드 호제법)
+	int remainder = 1;
+	while (remainder != 0) {
+		remainder = max % min;
+		max = min;
+		min = remainder;
+	}
+	return max;
+}
+
+int get_lcm(int min, int max) {		// 최소공배수 구하기  (최소공배수를 이용해 구한다.)
+	return min * max / get_gcd(min, max);
+}
+
+int main()
+{
+	int a, b, min, max;
+	printf("정수 2개입력: ");
+	scanf("%d %d", &a, &b);
+
+	if (a > b) {
+		max = a;
+		min = b;
+	}
+	else {
+		max = b;
+		min = a;
+	}
+
+	printf("최대공약수: %d\n", get_gcd(min, max));
+	printf("최소공배수: %d", get_lcm(min, max));
+}
